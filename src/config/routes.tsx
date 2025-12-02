9d95d79ec44f102c3aba43c3c70fe7296bfde087
wangpeng01 <wangpeng01@hetao101.com>
2025-12-02 18:26:27 +0800

    add: 换个名

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -39,7 +39,7 @@ export const routes: RouteConfig[] = [
   },
   {
     path: "/reactbook",
-    label: 'React "OS"',
+    label: 'React',
     element: <ReactBookPage />,
   },
   // {
--- src/pages/ReactBookPage.tsx
+++ src/pages/ReactBookPage.tsx
@@ -1,7 +1,7 @@
 export default function ReactBookPage() {
   return (
     <div className="space-y-4">
-      <h1 className="text-2xl font-bold">React OS</h1>
+      <h1 className="text-2xl font-bold">React设计模式</h1>
     </div>
   )
 }
